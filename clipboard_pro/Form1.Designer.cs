6471e7eeba8edb0ad44367b21db7a114130cf41b
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 10:50:07 +0530

    Fazrin

--- clipboard_pro/Form1.Designer.cs
+++ clipboard_pro/Form1.Designer.cs
@@ -41,9 +41,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 38);
+            this.button1.Location = new System.Drawing.Point(168, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 23);
+            this.button1.Size = new System.Drawing.Size(208, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Copy to Clipboard";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,24 +52,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(78, 12);
+            this.textBox1.Location = new System.Drawing.Point(104, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.Size = new System.Drawing.Size(337, 22);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Hi;I\'m first clipboard and i\'m happy!";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(78, 81);
+            this.textBox2.Location = new System.Drawing.Point(104, 100);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(254, 20);
+            this.textBox2.Size = new System.Drawing.Size(337, 22);
             this.textBox2.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(126, 107);
+            this.button2.Location = new System.Drawing.Point(168, 132);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 23);
+            this.button2.Size = new System.Drawing.Size(208, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Read from Clipboard";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,9 +80,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(168, 272);
+            this.button3.Location = new System.Drawing.Point(224, 335);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(137, 41);
+            this.button3.Size = new System.Drawing.Size(183, 50);
             this.button3.TabIndex = 4;
             this.button3.Text = "Copy image to clipborad";
             this.button3.UseVisualStyleBackColor = true;
@@ -87,18 +92,20 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(163, 151);
+            this.pictureBox1.Location = new System.Drawing.Point(217, 186);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(153, 105);
+            this.pictureBox1.Size = new System.Drawing.Size(203, 129);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(143, 319);
+            this.button4.Location = new System.Drawing.Point(168, 403);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(205, 41);
+            this.button4.Size = new System.Drawing.Size(273, 50);
             this.button4.TabIndex = 6;
             this.button4.Text = "Read  image from clipborad";
             this.button4.UseVisualStyleBackColor = true;
@@ -106,9 +113,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(188, 412);
+            this.button5.Location = new System.Drawing.Point(224, 474);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(117, 41);
+            this.button5.Size = new System.Drawing.Size(183, 50);
             this.button5.TabIndex = 7;
             this.button5.Text = "Clear Clipborad";
             this.button5.UseVisualStyleBackColor = true;
@@ -116,9 +124,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 476);
+            this.ClientSize = new System.Drawing.Size(596, 586);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox1);
@@ -127,6 +135,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
